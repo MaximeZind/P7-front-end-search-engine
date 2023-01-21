89d73d58ffbfc07c0b21cdeeee9013922397abe0
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-01-21 17:35:47 +0100

    Création des factories, des cartes de recette, et le dropdown ingrédient contient la liste des ingrédients

---
+++ factory/index.js
@@ -0,0 +1,113 @@
+// Fonction factory, qui va utiliser les données reçues de recipes.js
+// pour créer les éléments de notre page
+
+function recipePageFactory(data){
+    const {id, name, servings, ingredients, time, description, appliance, ustensils} = data;
+
+    //Fonction qui va créer nos cartes de recettes
+    function getRecipeCardsDOM() {
+        const article = document.createElement('article');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', 'https://via.placeholder.com/380x178?text=Lespetitsplats.com');
+
+        const divContent = document.createElement('div');
+        divContent.setAttribute('class', 'card__content');
+
+        const header = document.createElement('header');
+        const h2Title = document.createElement('h2');
+        h2Title.textContent = `${name}`;
+
+        const divTime = document.createElement('div');
+        divTime.setAttribute('class', 'card__header-time');
+
+
+        const iClock = document.createElement('i');
+        iClock.setAttribute('class', 'fa-regular fa-clock');
+
+        const strongTime = document.createElement('strong');
+        strongTime.textContent = `${time}`;
+
+        const divTextContent = document.createElement('div');
+        divTextContent.setAttribute('class', 'card__textcontent');
+
+        const divIngredients = document.createElement('div');
+        divIngredients.setAttribute('class', 'card__textcontent__ingredientslist');
+
+        ingredients.forEach((item) => {
+
+            const quantity = item.quantity;
+            const ingredient = item.ingredient;
+            let unit = '';
+            if (item.unit){
+                unit = item.unit
+            }
+            const pQuantity = document.createElement('p');
+            const strong = document.createElement('strong');
+            if (item.quantity && item.unit){
+                pQuantity.textContent = `${quantity} ${unit}`;
+                strong.textContent = `${ingredient}: `;
+            } else if (item.quantity && !item.unit){
+                 pQuantity.textContent = `${quantity}`;
+                 strong.textContent = `${ingredient}: `;
+            } else if (!item.quantity){
+                pQuantity.textContent = '';
+                strong.textContent = `${ingredient}`;
+            }
+        
+
+
+        
+            divIngredients.append(pQuantity);
+            pQuantity.prepend(strong);
+        
+         });
+        
+
+        const divInstructions = document.createElement('div');
+        divInstructions.setAttribute('class', 'card__textcontent__instructions');
+
+        const pInstructions = document.createElement('p');
+        pInstructions.textContent = `${description}`;
+
+        article.append(img);
+        article.append(divContent);
+        divContent.append(header);
+        header.append(h2Title);
+        header.append(divTime);
+        divTime.append(iClock);
+        divTime.append(strongTime);
+        divContent.append(divTextContent);
+        divTextContent.append(divIngredients);
+        divTextContent.append(divInstructions);
+        divInstructions.append(pInstructions);
+
+        return article
+
+    }
+
+    return {id, name, servings, time, description, appliance, ustensils, getRecipeCardsDOM}
+}
+
+
+//Fonction qui utilise les Array d'ingredients, appliances ou ustensils
+// pour créer les dropdown menus filtres
+function dropdownMenusFactory(ingredients, appliances, ustensils) {
+
+    function getIngredientsDrowndownDOM(){
+
+        const ul = document.createElement('ul');
+
+        ingredients.forEach((item) => {
+
+            let itemFirstCaseHigher = item.charAt(0).toUpperCase() + item.slice(1);
+            const li = document.createElement('li');
+            li.textContent = `${itemFirstCaseHigher}`;
+            ul.append(li);
+        });
+
+        return ul
+    }
+
+    return {getIngredientsDrowndownDOM}
+}
--- script/index.js
+++ script/index.js
@@ -1,14 +1,58 @@
 import {recipes} from '/data/recipes.js';
 
-function displayData(data){
+function displayData(recipes){
     const ingredientsList = document.querySelector(".ingredients__menu__list");
     const appareilsList = document.querySelector(".appareils__menu__list");
     const ustensilsList = document.querySelector(".ustensils__menu__list");
-    const recipesGallery = document.querySelector(".recipes__gallery")
+    const recipesGallery = document.querySelector(".recipes__gallery");
+
+    let allIngredients = [];
+    let allAppliances = [];
+    let allUstensils = [];
+
+    recipes.forEach((recipe) => {
+
+        recipe.ingredients.forEach((item) => {
+            let itemLowerCase = item.ingredient.toLowerCase();
+            if (!allIngredients.includes(itemLowerCase)){
+                allIngredients.push(itemLowerCase);
+            }
+        });
+
+            if (!allAppliances.includes(recipe.appliance)){
+                allAppliances.push(recipe.appliance);
+            }
+
+            recipe.ustensils.forEach((item) => {
+                if (!allUstensils.includes(item)){
+                    allUstensils.push(item);
+                }
+            });
+
+        const recipeModel = recipePageFactory(recipe);
+        const recipeCardsDOM = recipeModel.getRecipeCardsDOM();
+
+        recipesGallery.append(recipeCardsDOM);
+
+    });
+
+    const dropdownsModel = dropdownMenusFactory(allIngredients, allAppliances, allUstensils);
+    const IngredientsDrowndownDOM = dropdownsModel.getIngredientsDrowndownDOM();
+
+    ingredientsList.append(IngredientsDrowndownDOM);
+
+    // console.log(allIngredients);
+    // console.log(allAppliances);
+    // console.log(allUstensils);
+}
+
+function vw(x){
+    return ((x*100)/1440)
 }
 
 function init(){
-displayData(recipes)
+displayData(recipes);
+console.log(vw(667))
 }
 
 init();

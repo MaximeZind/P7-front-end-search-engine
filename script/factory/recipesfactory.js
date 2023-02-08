e1cca5c05e3cce3c0d576f03bbef25241bdf905b
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-02-08 15:48:53 +0100

    simplification de la factory

--- script/factory/recipesfactory.js
+++ script/factory/recipesfactory.js
@@ -8,79 +8,30 @@ export function recipesFactory(data){
     function getRecipeCardsDOM() {
         const article = document.createElement('article');
 
-        const img = document.createElement('img');
-        img.setAttribute('src', 'https://via.placeholder.com/380x178?text=Lespetitsplats.com');
-
-        const divContent = document.createElement('div');
-        divContent.setAttribute('class', 'card__content');
-
-        const header = document.createElement('header');
-        const h2Title = document.createElement('h2');
-        h2Title.textContent = `${name}`;
-
-        const divTime = document.createElement('div');
-        divTime.setAttribute('class', 'card__header-time');
-
-
-        const iClock = document.createElement('i');
-        iClock.setAttribute('class', 'fa-regular fa-clock');
-
-        const strongTime = document.createElement('strong');
-        strongTime.textContent = `${time}`;
-
-        const divTextContent = document.createElement('div');
-        divTextContent.setAttribute('class', 'card__textcontent');
-
-        const divIngredients = document.createElement('div');
-        divIngredients.setAttribute('class', 'card__textcontent__ingredientslist');
-
-        ingredients.forEach((item) => {
-
-            const quantity = item.quantity;
-            const ingredient = item.ingredient;
-            let unit = '';
-            if (item.unit){
-                unit = item.unit
-            }
-            const pQuantity = document.createElement('p');
-            const strong = document.createElement('strong');
-            if (item.quantity && item.unit){
-                pQuantity.textContent = `${quantity} ${unit}`;
-                strong.textContent = `${ingredient}: `;
-            } else if (item.quantity && !item.unit){
-                 pQuantity.textContent = `${quantity}`;
-                 strong.textContent = `${ingredient}: `;
-            } else if (!item.quantity){
-                pQuantity.textContent = '';
-                strong.textContent = `${ingredient}`;
-            }
-        
-
-
-        
-            divIngredients.append(pQuantity);
-            pQuantity.prepend(strong);
-        
-         });
-        
-
-        const divInstructions = document.createElement('div');
-        divInstructions.setAttribute('class', 'card__textcontent__instructions');
-
-        const pInstructions = document.createElement('p');
-        pInstructions.textContent = `${description}`;
-
-        article.append(img);
-        article.append(divContent);
-        divContent.append(header);
-        header.append(h2Title);
-        header.append(divTime);
-        divTime.append(iClock);
-        divTime.append(strongTime);
-        divContent.append(divTextContent);
-        divTextContent.append(divIngredients);
-        divTextContent.append(divInstructions);
-        divInstructions.append(pInstructions);
+        article.innerHTML = `
+        <img src="https://via.placeholder.com/380x178?text=Lespetitsplats.com">
+        <div class="card__content">
+            <header>
+                <h2>${name}</h2>
+                <div class="card__header-time"><i class="fa-regular fa-clock"></i><strong>${time}</strong></div>
+            </header>
+            <div class="card__textcontent">
+                <div class="card__textcontent__ingredientslist">
+                ${ingredients.map(item => `
+                <p>
+                    <strong>
+                        ${item.ingredient}:
+                        ${item.quantity ? `${item.quantity} ${item.unit || ''}` : ''}
+                    </strong>
+                </p>
+            `).join('')}
+                </div>
+                <div class="card__textcontent__instructions">
+                    <p>${description}</p>
+                </div>
+            </div>
+        </div>
+        `
 
         return article
 

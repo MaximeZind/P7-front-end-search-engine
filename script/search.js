d74a224b9be49f474df03ff555414b8189580163
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-02-08 22:21:47 +0100

    globals.js supprimée, et sa fonction se retrouve dans search.js

--- script/search.js
+++ script/search.js
@@ -33,6 +33,38 @@ export function getInputKeywords() {
     return keywords;
 }
 
+//Fonction qui renvoie une array de mots clés, à partir d'une recette
+export function getRecipeArray(recipe) {
+    const stopWords = ['de', 'un', 'une', 'le', 'la', 'les', 'et', 'en', 'du', 'au', 'a', '0', '1', '2', '3', 'ou'];
+    let ingredients = [];
+    recipe.ingredients.forEach((ingredient) => {
+        ingredients = ingredients.concat(ingredient.ingredient.toLowerCase().split(' '));
+
+    });
+
+    let ustensils = [];
+    recipe.ustensils.forEach((ustensil) => {
+        ustensils = ustensils.concat(ustensil.toLowerCase().split(' '));
+
+    });
+    
+    let instructions = recipe.description.toLowerCase().split(' ');
+
+    let appliance = recipe.appliance.toLowerCase();
+
+    let title = recipe.name.toLowerCase().split(' ');
+
+    let keywords = title.concat(ingredients)
+        .concat(instructions)
+        .concat(appliance)
+        .concat(ustensils)
+        .filter(word => !stopWords.includes(word))
+        .filter(element => element);
+    return keywords;
+}
+
+
+//Fonction qui va filtrer les recettes en fonction de l'input
 export function search() {
     const inputKeywords = getInputKeywords();
     const allFilters = inputKeywords.ingredientsTags.concat(inputKeywords.ustensilsTags).concat(inputKeywords.appliancesTags);
